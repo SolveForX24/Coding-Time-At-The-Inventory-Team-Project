d44f36f68e05f60c1b502a996aa7cec5fe803510
Matthew Weir <matthew.weir@mcvts.org>
2022-03-28 10:44:07 -0400

    sus

---
+++ Coding Time At The Inventory Team Project/Coding Time At The Inventory Team Project/FileData.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "FileData.h"
+
+void readFromFile(vector<vector<string>>& inventory) {
+    ifstream fin("data.txt");
+
+    if (fin.good()) {
+        int i = 0;
+        vector<string> currentItem;
+        string line;
+
+        while (fin.peek() != EOF) {
+            getline(fin, line);
+
+            if (line == "") {
+                i = 0;
+                inventory.push_back(currentItem);
+                currentItem = vector<string>();
+                continue;
+            }
+
+            currentItem.push_back(line);
+        }
+    }
+
+    fin.close();
+}
+
+void writeToFile(vector<vector<string>>& inventory) {
+    ofstream fout("data.txt");
+
+    for (int i = 0; i < inventory.size(); i++) {
+        for (int j = 0; j < inventory[i].size(); j++) {
+            fout << inventory[i][j] << endl;
+        }
+        fout << endl;
+    }
+
+    fout.close();
+}
---
+++ Coding Time At The Inventory Team Project/Coding Time At The Inventory Team Project/FileData.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void readFromFile(vector<vector<string>>&);
+void writeToFile(vector<vector<string>>&);
