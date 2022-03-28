b02e8b457034a4a8a79e991fbd4da4705f2d81f4
SolveForX24 <solveforxnj@gmail.com>
2022-03-28 10:41:44 -0400

    lot of createItem function done, still needs shelf, sale and comments.

---
+++ Coding Time At The Inventory Team Project/Coding Time At The Inventory Team Project/solveFunctions.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip> 
+
+#include "GetData.h"
+
+#include "solveFunctions.h"
+using namespace std;
+
+void createItem(vector<vector<string>> inven) {
+	
+	/*
+		Name
+		ID
+		DONAR
+
+		Price
+		Stock
+		Department
+		Shelf
+		Sale
+	*/
+
+	vector<string> item;
+	
+	string prompt;
+	string dummy;
+
+	int intPrompt;
+
+	double doubPrompt;
+
+	cout << "Please enter the item name: ";
+	getline(cin, prompt);
+	item.push_back(prompt);
+	getline(cin, dummy);
+
+	string prompt = to_string(inven.size());
+
+	item.push_back(prompt);
+
+	do {
+		cout << "Did someone donate this " + item[0] + "? (y/n) ";
+		getline(cin, prompt);
+		getline(cin, dummy);
+	} while (prompt != "y" && prompt != "Y" && prompt != "n" && prompt != "N");
+
+	if (prompt == "Y" || prompt == "y") {
+		cout << "Please enter the name of the person who donated this: ";
+		getline(cin, prompt);
+		getline(cin, dummy);
+		item.push_back(prompt);
+	}
+	else {
+		item.push_back("N/A");
+	}
+
+	do {
+		cout << "Please enter the price of the item: ";
+	} while (!getValidDouble(doubPrompt) && doubPrompt < 0);
+
+	item.push_back(to_string(doubPrompt));
+
+	do {
+		cout << "Please enter how many of the item we have received: ";
+	} while (!getValidInt(intPrompt) && intPrompt < 1);
+
+	item.push_back(to_string(intPrompt));
+
+	cout << "Department 1: Clothes\n"
+		<< "Department 2: Electronics\n"
+		<< "Department 3: Food\n"
+		<< "Department 4: Health Care\n"
+		<< "Department 5: Skincare\n";
+
+	do {
+		cout << "Please enter the department the item belongs to (1-5): ";
+	} while (!getValidInt(intPrompt) && intPrompt < 1 && intPrompt > 5);
+
+	item.push_back(to_string(intPrompt));
+
+	//return item;
+}
--- Coding Time At The Inventory Team Project/Coding Time At The Inventory Team Project/solveFunctions.h
+++ Coding Time At The Inventory Team Project/Coding Time At The Inventory Team Project/solveFunctions.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void createItem(vector<vector<string>>);
